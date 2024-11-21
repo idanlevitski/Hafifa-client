29419f57aee75b7fb39f7b719f7490916c6dde29
idanlevitski <idanlevitski@gmail.com>
2024-11-21 12:58:06 +0200

    feat(App.tsx): page elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,46 @@
+import { useState } from "react";
 import "./App.css";
+import { Box, Button, Typography } from "@mui/material";
 
 function App() {
-  return <></>;
+  const inputs = [];
+  const NUM_OF_DIGITS: number = 8;
+  const [id, setId] = useState("");
+
+  const updateId = () => {
+    setId("");
+    // inputs.forEach((input) => {
+    //   input;
+    // });
+  };
+
+  for (let index = 0; index < NUM_OF_DIGITS; index++) {
+    inputs.push(
+      <input className="input" onInput={updateId} maxLength={1}></input>
+    );
+  }
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        mt: "25vh",
+        gap: "3vh",
+      }}
+    >
+      <Typography sx={{ fontSize: "70px" }}>Enter 8 digits</Typography>
+      <Box>{inputs}</Box>
+      <Button
+        variant="outlined"
+        sx={{ fontSize: "1.5vw" }}
+        disabled={id.length !== NUM_OF_DIGITS}
+      >
+        Send
+      </Button>
+    </Box>
+  );
 }
 
 export default App;
