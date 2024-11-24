7b81b3739ba201701f810dceb03ebb927c276414
idanlevitski <idanlevitski@gmail.com>
2024-11-24 11:08:53 +0200

    refactor(App.tsx): delete unused const and add utils files

--- src/App.tsx
+++ src/App.tsx
@@ -5,52 +5,28 @@ import { getLastDigit } from "./axios/useAxios";
 function App() {
   const NUM_OF_DIGITS: number = 8;
 
-  const inputs: JSX.Element[] = [];
   const [values, setValues] = useState<string[]>(
     new Array(NUM_OF_DIGITS).fill("")
   );
   const [lastDigit, setLastDigit] = useState<number | undefined>(undefined);
 
   const handleInput = (e: KeyboardEvent<HTMLInputElement>) => {
-    let shift: number = 0;
     const elementIndex = parseInt((e.target as HTMLInputElement).id);
 
     const newValues: string[] = [...values];
 
     if (/^\d$/.test(e.key)) {
-      shift = 1;
-
       newValues[elementIndex] = e.key;
       setValues(newValues);
-    } else if (e.key === "Backspace") {
-      shift = -1;
 
+      document.getElementById(`${elementIndex + 1}`)?.focus();
+    } else if (e.key === "Backspace") {
       newValues[elementIndex] = "";
       setValues(newValues);
-    } else {
-      e.preventDefault();
+
+      document.getElementById(`${elementIndex - 1}`)?.focus();
     }
-
-    document.getElementById(`${elementIndex + shift}`)?.focus();
   };
-
-  for (let index = 0; index < NUM_OF_DIGITS; index++) {
-    inputs.push(
-      <TextField
-        sx={{
-          width: "5vh",
-          mr: "1vw",
-          borderColor: "#0066ff",
-        }}
-        onKeyDown={handleInput}
-        id={`${index}`}
-        key={index}
-        slotProps={{ htmlInput: { maxLength: 1 } }}
-        value={values[index]}
-        className="inputBox"
-      ></TextField>
-    );
-  }
 
   const handleClear = () => {
     setValues(new Array(NUM_OF_DIGITS).fill(""));
@@ -77,7 +53,25 @@ function App() {
       >
         {`Enter ${NUM_OF_DIGITS} digits`}
       </Typography>
-      <Box>{inputs}</Box>
+      <Box>
+        {new Array(NUM_OF_DIGITS).fill(undefined).map((_, index) => {
+          return (
+            <TextField
+              sx={{
+                width: "5vh",
+                mr: "1vw",
+                borderColor: "#0066ff",
+              }}
+              onKeyDown={handleInput}
+              id={`${index}`}
+              key={index}
+              slotProps={{ htmlInput: { maxLength: 1 } }}
+              value={values[index]}
+              className="inputBox"
+            ></TextField>
+          );
+        })}
+      </Box>
       <Box sx={{ display: "flex", flexDirection: "row", gap: "1vw" }}>
         <Button
           variant="outlined"
@@ -111,7 +105,7 @@ function App() {
         </Button>
       </Box>
 
-      {lastDigit && <Typography>{lastDigit}</Typography>}
+      <Typography>{lastDigit}</Typography>
     </Box>
   );
 }
